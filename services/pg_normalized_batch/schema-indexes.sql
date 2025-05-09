792316657eb29d054fdb5191e55f0dd345f4c2f2
stell25 <scheng25@students.claremontmckenna.edu>
2025-05-08 20:17:28 -0700

    edited schema normalized

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,11 +1,11 @@
-CREATE INDEX tweet_tags_tag_id_tweets_btree_idx ON tweet_tags USING btree (tag, id_tweets);
+CREATE INDEX tweet_tags_tag_id_tweets_btree_idx ON tweet_tags(tag, id_tweets);
 
-CREATE INDEX tweet_tags_id_tweets_tag_idx ON tweet_tags USING btree (id_tweets, tag);
+CREATE INDEX tweet_tags_id_tweets_tag_idx ON tweet_tags(id_tweets, tag);
 
-CREATE INDEX tweets_to_tsvector_en_idx ON tweets USING gin (to_tsvector('english', text)) WHERE lang = 'en';
+CREATE INDEX tweets_to_tsvector_en_idx ON tweets(to_tsvector('english', text)) WHERE lang = 'en';
 
-CREATE INDEX tweets_id_tweets_lang_idx ON tweets USING btree (id_tweets, lang);
+CREATE INDEX tweets_id_tweets_lang_idx ON tweets(id_tweets, lang);
 
-CREATE INDEX tweets_lang_idx ON tweets USING btree (lang);
+CREATE INDEX tweets_lang_idx ON tweets(lang);
 
-CREATE INDEX tweet_tags_tag_text_pattern_ops_id_tweets_idx ON tweet_tags USING btree (tag text_pattern_ops, id_tweets);
+CREATE INDEX tweet_tags_tag_text_pattern_ops_id_tweets_idx ON tweet_tags(tag text_pattern_ops, id_tweets);
